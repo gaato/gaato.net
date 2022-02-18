0c0836b80b9a5cde3c5d426ebe75de907dd2ab9a
gaato <gart@outlook.jp>
2022-02-18 16:57:04 +0900

    Add pages

--- homepage/urls.py
+++ homepage/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'homepage'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('profile', views.ProfileView.as_view(), name='profile'),
     path('tex', views.TexView.as_view(), name='tex'),
+    path('hololive', views.HololiveView.as_view(), name='hololive'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,15 @@
+from re import template
 from django.views import generic
 
 
 class IndexView(generic.TemplateView):
-    template_name = "index.html"
+    template_name = 'index.html'
+
+class ProfileView(generic.TemplateView):
+    template_name = 'profile.html'
 
 class TexView(generic.TemplateView):
-    template_name = "tex.html"
+    template_name = 'tex.html'
+
+class HololiveView(generic.TemplateView):
+    template_name = 'hololive.html'
